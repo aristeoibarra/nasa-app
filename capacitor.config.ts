30d3cbfb53fe5aa20dff2376a726176b8ba5e23b
aristeoibarra <aristeoibarra608@gmail.com>
2022-09-21 02:16:52 -0600

    initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from "@capacitor/cli";
+
+const config: CapacitorConfig = {
+  appId: "io.ionic.starter",
+  appName: "Nasa Api",
+  webDir: "build",
+  bundledWebRuntime: false,
+};
+
+export default config;
---
+++ src/components/Menu/index.tsx
@@ -0,0 +1,113 @@
+import {
+  IonContent,
+  IonIcon,
+  IonItem,
+  IonLabel,
+  IonList,
+  IonListHeader,
+  IonMenu,
+  IonMenuToggle,
+  IonNote,
+} from "@ionic/react";
+
+import { useLocation } from "react-router-dom";
+import {
+  archiveOutline,
+  archiveSharp,
+  bookmarkOutline,
+  heartOutline,
+  heartSharp,
+  mailOutline,
+  mailSharp,
+  paperPlaneOutline,
+  paperPlaneSharp,
+  trashOutline,
+  trashSharp,
+  warningOutline,
+  warningSharp,
+} from "ionicons/icons";
+import "./Menu.css";
+
+interface AppPage {
+  url: string;
+  iosIcon: string;
+  mdIcon: string;
+  title: string;
+}
+
+const appPages: AppPage[] = [
+  {
+    title: "Inbox",
+    url: "/",
+    iosIcon: mailOutline,
+    mdIcon: mailSharp,
+  },
+  {
+    title: "Outbox",
+    url: "/outbox",
+    iosIcon: paperPlaneOutline,
+    mdIcon: paperPlaneSharp,
+  },
+  {
+    title: "Favorites",
+    url: "/favorites",
+    iosIcon: heartOutline,
+    mdIcon: heartSharp,
+  },
+  {
+    title: "Archived",
+    url: "/archived",
+    iosIcon: archiveOutline,
+    mdIcon: archiveSharp,
+  },
+  {
+    title: "Trash",
+    url: "/trash",
+    iosIcon: trashOutline,
+    mdIcon: trashSharp,
+  },
+  {
+    title: "Spam",
+    url: "/spam",
+    iosIcon: warningOutline,
+    mdIcon: warningSharp,
+  },
+];
+
+const Menu: React.FC = () => {
+  const location = useLocation();
+
+  return (
+    <IonMenu contentId="main" type="overlay">
+      <IonContent>
+        <IonList id="inbox-list">
+          <IonListHeader>NASA API</IonListHeader>
+          {appPages.map((appPage, index) => {
+            return (
+              <IonMenuToggle key={index} autoHide={false}>
+                <IonItem
+                  className={
+                    location.pathname === appPage.url ? "selected" : ""
+                  }
+                  routerLink={appPage.url}
+                  routerDirection="none"
+                  lines="none"
+                  detail={false}
+                >
+                  <IonIcon
+                    slot="start"
+                    ios={appPage.iosIcon}
+                    md={appPage.mdIcon}
+                  />
+                  <IonLabel>{appPage.title}</IonLabel>
+                </IonItem>
+              </IonMenuToggle>
+            );
+          })}
+        </IonList>
+      </IonContent>
+    </IonMenu>
+  );
+};
+
+export default Menu;
